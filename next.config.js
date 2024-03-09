68e7a5208d92757119d5f97a085df2bf2cc1badc
Marwan Hisham <marwanhiisham@outlook.com>
2024-03-10 01:45:34 +0200

    uncommeted

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "http",
-        hostname: "digital-hippo-nine.vercel.app",
+        hostname: "localhost",
       },
       {
         protocol: "https",
