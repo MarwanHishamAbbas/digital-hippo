18f1609096e04e4178c37b6a3cb7c26cab6fb734
Marwan Hisham <marwanhiisham@outlook.com>
2024-03-10 01:19:21 +0200

    uncommeted

--- src/server.ts
+++ src/server.ts
@@ -30,26 +30,26 @@ export type WebhookRequest = IncomingMessage & {
 }
 
 const start = async () => {
-  // const webhookMiddleware = bodyParser.json({
-  //   verify: (req: WebhookRequest, _: any, buffer: any) => {
-  //     req.rawBody = buffer
-  //   },
-  // })
+  const webhookMiddleware = bodyParser.json({
+    verify: (req: WebhookRequest, _: any, buffer: any) => {
+      req.rawBody = buffer
+    },
+  })
 
-  // app.post("/api/webhooks/stripe", webhookMiddleware, stripeWebhookHandler)
+  app.post("/api/webhooks/stripe", webhookMiddleware, stripeWebhookHandler)
 
-  // const payload = await getPayloadClient({
-  //   initOptions: {
-  //     express: app,
-  //     onInit: async (cms) => {
-  //       cms.logger.info(`Admin URL: ${cms.getAdminURL()}`)
-  //     },
-  //   },
-  // })
+  const payload = await getPayloadClient({
+    initOptions: {
+      express: app,
+      onInit: async (cms) => {
+        cms.logger.info(`Admin URL: ${cms.getAdminURL()}`)
+      },
+    },
+  })
 
   if (process.env.NEXT_BUILD) {
     app.listen(PORT, async () => {
-      // payload.logger.info("Next.js is building for production")
+      payload.logger.info("Next.js is building for production")
 
       // @ts-expect-error
       await nextBuild(path.join(__dirname, "../"))
@@ -60,22 +60,22 @@ const start = async () => {
     return
   }
 
-  // const cartRouter = express.Router()
+  const cartRouter = express.Router()
 
-  // cartRouter.use(payload.authenticate)
+  cartRouter.use(payload.authenticate)
 
-  // cartRouter.get("/", (req, res) => {
-  //   const request = req as PayloadRequest
+  cartRouter.get("/", (req, res) => {
+    const request = req as PayloadRequest
 
-  //   if (!request.user) return res.redirect("/sign-in?origin=cart")
+    if (!request.user) return res.redirect("/sign-in?origin=cart")
 
-  //   const parsedUrl = parse(req.url, true)
-  //   const { query } = parsedUrl
+    const parsedUrl = parse(req.url, true)
+    const { query } = parsedUrl
 
-  //   return nextApp.render(req, res, "/cart", query)
-  // })
+    return nextApp.render(req, res, "/cart", query)
+  })
 
-  // app.use("/cart", cartRouter)
+  app.use("/cart", cartRouter)
   app.use(
     "/api/trpc",
     trpcExpress.createExpressMiddleware({
@@ -86,15 +86,15 @@ const start = async () => {
 
   app.use((req, res) => nextHandler(req, res))
 
-  // nextApp.prepare().then(() => {
-  //   payload.logger.info("Next.js started")
+  nextApp.prepare().then(() => {
+    payload.logger.info("Next.js started")
 
-  //   app.listen(PORT, async () => {
-  //     payload.logger.info(
-  //       `Next.js App URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`
-  //     )
-  //   })
-  // })
+    app.listen(PORT, async () => {
+      payload.logger.info(
+        `Next.js App URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`
+      )
+    })
+  })
 }
 
 start()
